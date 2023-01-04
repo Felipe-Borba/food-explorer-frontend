ab9aeacc1b139949b883d1f6aca466f091c12ca5
Felipe Borba <felipe.borba0109@gmail.com>
2023-01-03 22:28:44 -0300

    start view dish layout

--- src/pages/dish/View/index.tsx
+++ src/pages/dish/View/index.tsx
@@ -1,10 +1,116 @@
-import React from "react";
+import {
+  Flex,
+  HStack,
+  Heading,
+  Image,
+  SkeletonCircle,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { ButtonPrimary } from "../../../components/global/butons/ButtonPrimary";
+import { ButtonText } from "../../../components/global/butons/ButtonText";
+import { IconArrowLeft } from "../../../components/icons/IconArrowLeft";
+import { MainLayout } from "../../../components/layouts/MainLayout";
+import { api } from "../../../services/api";
+const baseURL = import.meta.env.VITE_API_URL;
 
 export default function DishView() {
   const navigate = useNavigate();
   const params = useParams();
   const { id } = params;
 
-  return <div>index{id}</div>;
+  const quantity = 0;
+
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    async function fetchDish() {
+      try {
+        const response = await api.get(`/dish/${id}`);
+        const { dish, ingredients } = response.data;
+
+        setImage(`${baseURL}/files/${dish.image}`);
+        setName(dish.name);
+        setPrice(dish.price);
+        setDescription(dish.description);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchDish();
+  }, [id]);
+
+  return (
+    <MainLayout>
+      <VStack mx="122px" gap="42px" alignItems={"flex-start"}>
+        <ButtonText fontSize="24px" my="32px" onClick={() => navigate(-1)}>
+          <IconArrowLeft mr="11px" />
+          Voltar
+        </ButtonText>
+
+        <HStack gap="41px" alignItems={"flex-start"}>
+          <SkeletonCircle
+            isLoaded={Boolean(image)}
+            boxSize={"389.47px"}
+            overflow="hidden"
+          >
+            <Image objectFit="cover" src={image} alt="Foto do prato" />
+          </SkeletonCircle>
+
+          <Flex flexDir={"column"}>
+            <Heading
+              fontFamily={"Poppins"}
+              fontWeight="500"
+              fontSize={"40px"}
+              color="#E1E1E6"
+            >
+              {name}
+            </Heading>
+
+            <Text
+              mt="8px"
+              fontFamily={"Poppins"}
+              fontWeight="400"
+              fontSize={"24px"}
+              color="#E1E1E6"
+            >
+              {description}
+            </Text>
+
+            {/* //TODO ingredients container */}
+
+            <HStack mt="43px">
+              <Heading fontSize={"32px"} fontWeight="400" color={"#82F3FF"}>
+                R$ {price.toString().replace(".", ",")}
+              </Heading>
+
+              <Flex
+                alignItems={"center"}
+                gap="5px"
+                pl={"54px"}
+                pr="27px"
+                fontSize="20px"
+              >
+                <ButtonText _hover={{ bgColor: "#c4c4cc5e" }} fontSize="20px">
+                  -
+                </ButtonText>
+                <Text>{quantity}</Text>
+                <ButtonText _hover={{ bgColor: "#c4c4cc5e" }} fontSize="20px">
+                  +
+                </ButtonText>
+              </Flex>
+
+              <ButtonPrimary>incluir</ButtonPrimary>
+            </HStack>
+          </Flex>
+        </HStack>
+      </VStack>
+    </MainLayout>
+  );
 }
